043d9965922e042ef9a5f32beea2dd27a068f0f8
Allen <allenonrails@zohomail.eu>
2021-11-20 12:55:08 +0300

    final

--- js/main.js
+++ js/main.js
@@ -21,3 +21,104 @@ function burgerToggleAnimation(){
 }
 
 burgerToggleAnimation()
+
+
+// filter list li click events
+
+function fliterCheckboxEvents(){
+  let checkboxItems = document.querySelectorAll('.filter-list__item');
+
+  checkboxItems.forEach(li => {
+    li.querySelector('.checkbox-item').addEventListener('click', function(){
+      let input = li.querySelector('.filter-list__input');
+      this.parentElement.classList.toggle('checkbox-active')
+      if (input.checked == false){
+        input.checked = 'true'
+        numCounterInTheFilterUl()
+      } else if (input.checked){
+        input.checked = ''
+        numCounterInTheFilterUl()
+      }
+      
+    })
+  });
+}
+
+// btn filter reset
+
+function fliterCheckboxActiveReset(){
+  document.querySelector('.calendar-filter__btn').addEventListener('click', function(){
+    document.querySelectorAll('.filter-list__item').forEach(li => {
+      let input = li.querySelector('.filter-list__input');
+      if (li.classList.contains('checkbox-active')){
+        input.checked = ''
+        li.classList.remove('checkbox-active')
+        numCounterInTheFilterUl()
+      }
+    });
+  })
+}
+
+fliterCheckboxEvents()
+fliterCheckboxActiveReset()
+
+// filter list header num counter
+
+function numCounterInTheFilterUl(){
+  document.querySelectorAll('.filter-list').forEach(list => {
+    let countActiveItems = list.querySelectorAll('.checkbox-active').length;
+    list.querySelector('.filter-list__title > span').innerText = countActiveItems
+  })
+}
+
+numCounterInTheFilterUl()
+
+// modal filter
+
+function filterModal(){
+  document.querySelector('.calendar__filter-btn').addEventListener('click', function(){
+    document.querySelector('body').classList.add('hidden')
+    document.querySelector('.mask').style.display = 'block'
+    document.querySelector('.calendar__filter-block').style.display = 'block'
+    document.querySelector('.filter-modal-cross').addEventListener('click', function(){
+      document.querySelector('body').classList.remove('hidden')
+      document.querySelector('.mask').style.display = 'none'
+      document.querySelector('.calendar__filter-block').style.display = 'none'
+    })
+  })
+}
+
+filterModal()
+
+// modal calendar day
+
+function calendarDayModal(){
+  document.querySelectorAll('.calendar__day').forEach(block => {
+    block.addEventListener('click', function(){
+      let modal = document.querySelector('.calendar-block-modal');
+
+      document.querySelector('body').classList.add('hidden')
+      document.querySelector('.mask').style.display = 'block'
+      modal.style.display = 'block'
+
+      // inner content
+      data = [
+        '.calendar-day__date',
+        '.calendar-day__time-nums',
+        '.calendar-day__title',
+        '.calendar-day__descrption'
+      ]
+      data.forEach(className => {
+        modal.querySelector(className).innerText = block.querySelector(className).textContent
+      });
+
+      document.querySelector('.calendar-block-modal__close').addEventListener('click', function(){
+        document.querySelector('body').classList.remove('hidden')
+        document.querySelector('.mask').style.display = 'none'
+        modal.style.display = 'none'
+      })
+    })
+  })
+}
+
+calendarDayModal()
